52d578f48834f7c25bc9247d4665fc16f8a7c274
Caspian <caspian.susskind@gmail.com>
2021-09-27 16:28:24 +0200

    feat: output greedy sampling

--- src/al.py
+++ src/al.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import spatial
 
 
 def max_std_sampling(pred, batchsize):
@@ -23,3 +24,32 @@ def max_std_sampling(pred, batchsize):
     error_msg = 'Batchsize must be smaller or equal to number of samples N'
     assert batchsize <= pred.shape[0], error_msg
     return np.argpartition(np.std(pred, axis=(1, 2)), -batchsize)[-batchsize:]
+
+
+unlabeled_output = np.array(
+    [[243,  3173],
+     [525,  2997]])
+
+labeled_data = np.array(
+    [[682, 2644],
+     [277, 2651],
+     [396, 2640]])
+
+
+def output_greedy_sampling(labeled_data, unlabeled_output, batch_size):
+    """
+    :param labeled_data: Labeled data points from storage
+    :param unlabeled_output: Unlabeled data points with predictions from ML model
+    :param batch_size: Number of points to query label for
+    :return: Indexes for the points to query on
+    """
+
+    # Compute the distance from all unlabeled output data to the closest labeled data point and assign minimum one to each point
+    min_distances = np.min(spatial.distance.cdist(
+        unlabeled_output, labeled_data), axis=1)
+    # Then select the batch_size number of samples that have the largest distance to label and return indices
+    return np.argpartition(min_distances, -batch_size)[-batch_size:]
+
+
+indices = output_greedy_sampling(labeled_data, unlabeled_output, 2)
+print(indices)
