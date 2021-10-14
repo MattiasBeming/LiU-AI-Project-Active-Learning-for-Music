b21a3cf38505d38e723ed1c76b29ab5fe39140f1
Ludvig Joborn <ludjo721@student.liu.se>
2021-10-14 15:03:11 +0200

    feat: load valence and arousal labels

--- src/transformer.py
+++ src/transformer.py
@@ -1,5 +1,7 @@
 import numpy as np  # Version 1.19.1
 from pathlib import Path
+import pandas as pd
+import warnings
 
 
 def load_songs(file_indexes=range(1, 1000)):
@@ -14,19 +16,45 @@ def load_songs(file_indexes=range(1, 1000)):
                 corresponding data file.
                 (Not all number from 1 to 1000 have a corresponding file.)
     """
-    data = np.empty((92, 6670, 1))
     found_inds = []
+    first = True
     for i in file_indexes:
         file_path = Path(
             f"./../../emo-music-features/default_features/{i}.csv")
         if file_path.is_file():
             found_inds.append(i)
-            data_temp = np.genfromtxt(file_path, delimiter=';')
+            data_temp = np.genfromtxt(file_path, delimiter=';', skip_header=1)
             data_temp = data_temp.reshape(
-                (data_temp.shape[0], data_temp.shape[1], 1))
-            data = np.concatenate((data, data_temp), axis=2)
+                (1, data_temp.shape[0], data_temp.shape[1]))
+            if first:
+                data = data_temp
+                first = False
+            else:
+                data = np.concatenate((data, data_temp), axis=0)
 
     return(data, found_inds)
+
+
+def load_annotations(path_arousal, path_valence):
+    """
+    Loads in valence and arousal labels from respective paths.
+    Input:
+        path_arousal: A Path object (from pathlib), containing
+            the path to "arousal_cont_average.csv".
+        path_valence: A Path object (from pathlib), containing
+            the path to "valence_cont_average.csv".
+    Output:
+        arousal: A 2D pandas object.
+        valence: A 2D pandas object.
+    """
+    if (path_arousal.is_file() and path_valence.is_file()):
+        arousal = pd.read_csv(path_arousal, delimiter=",")
+        valence = pd.read_csv(path_valence,  delimiter=",")
+        return (arousal, valence)
+    else:
+        warnings.warn(
+            "Warning: One or both of the file paths are incorrect.")
+        return(-1)
 
 
 def find_inds(file_indexes=range(1, 1000)):
@@ -47,9 +75,3 @@ def find_inds(file_indexes=range(1, 1000)):
         if file_path.is_file():
             found_inds.append(i)
     return found_inds
-
-
-# Run code
-data, inds = load_songs(range(1, 3))
-print(np.shape(data))
-print(len(find_inds()))
