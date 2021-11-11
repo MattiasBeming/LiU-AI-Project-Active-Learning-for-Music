5f8ee4e789afd49a7a63166cb35f73433a3997c8
Alfred Sporre <alfred@familjensporre.se>
2021-11-11 16:04:17 +0100

    feat: gui system

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from threading import Thread
+import gui
+
+#################
+# MAIN FUNCTION #
+#################
+
+
+def main():
+    pass
+
+
+#########################################
+# START APPLICATION & RUN MAIN FUNCTION #
+#########################################
+
+# Start application
+_app = Thread(target=main)
+print("Starting application thread...")
+_app.start()
+print("Application thread started!")
+
+# Drive GUI
+print("Entering GUI loop on main thread.")
+while _app.is_alive():
+    gui.update_windows()
+print("The GUI loop on main thread was exited " +
+      "since the application thread was stopped!")
+
+# Exit GUI
+print("Destroying GUI...")
+gui.destroy()
+print("The GUI was successfully destroyed!")
