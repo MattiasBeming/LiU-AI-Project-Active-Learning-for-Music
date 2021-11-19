6e621a650946fe896823ea2291de79ae7b7405f2
Alfred Sporre <alfred@familjensporre.se>
2021-11-19 11:12:36 +0100

    feat: post processing support for gui results
    also:
    * option for disabling native window closing
    * swap 'math' library for 'numpy'
    * forward __TIMEOUT__ events to window agents

--- src/windows.py
+++ src/windows.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-import math
+import numpy as np
 
 #############
 # CONSTANTS #
@@ -9,8 +9,8 @@ import math
 INPUT_RANGE = (-1, 1)  # Slider limits
 INPUT_RESOLUTION = .001  # Slider resolution
 INDICATOR_SIZE = 10
-CANVAS_WIDTH = 250
-CANVAS_HEIGHT = 200
+PRIOR_CANVAS_WIDTH = 250
+PRIOR_CANVAS_HEIGHT = 200
 EMO_LABELS = [
     "Excited", "Delighted", "Happy",
     "Content", "Relaxed", "Calm",
@@ -53,10 +53,22 @@ def _layout_query_prior(): return [
                 sg.Button("Reset", key="-RESET-")
             ]
         ]),
-        sg.Canvas(key="-CANVAS-", size=(CANVAS_WIDTH, CANVAS_HEIGHT),
-                  background_color="white")
+        sg.Canvas(
+            key="-CANVAS-",
+            size=(PRIOR_CANVAS_WIDTH, PRIOR_CANVAS_HEIGHT),
+            background_color="white"
+        )
     ]
 ]
+
+#####################
+# RESULT PROCESSORS #
+#####################
+
+
+def process_result_default(result):
+    return result
+
 
 ####################
 # EVENT PROCESSORS #
@@ -64,6 +76,10 @@ def _layout_query_prior(): return [
 
 
 def handle_query_prior(window, event, values, emotion_indicator):
+
+    # Skip if timeout
+    if event == sg.TIMEOUT_EVENT:
+        return False, None
 
     # Closure
     close = False
@@ -81,8 +97,8 @@ def handle_query_prior(window, event, values, emotion_indicator):
 
     # Calculate position of emotion indicator
     ei_pos = (
-        (values["-VALENCE-"] + 1) * CANVAS_WIDTH / 2,
-        (-values["-AROUSAL-"] + 1) * CANVAS_HEIGHT / 2
+        (values["-VALENCE-"] + 1) * PRIOR_CANVAS_WIDTH / 2,
+        (-values["-AROUSAL-"] + 1) * PRIOR_CANVAS_HEIGHT / 2
     )
 
     window["-CANVAS-"].TKCanvas.coords(
@@ -111,24 +127,24 @@ def init_query_prior():
     for i, lab in enumerate(EMO_LABELS):
 
         # Calculate geometries
-        c = (CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2)  # Center
-        r = CANVAS_HEIGHT * .45  # Radius
-        a = math.pi / 12 * (2 * i + 1)  # Angle
+        c = (PRIOR_CANVAS_WIDTH / 2, PRIOR_CANVAS_HEIGHT / 2)  # Center
+        r = PRIOR_CANVAS_HEIGHT * .45  # Radius
+        a = np.pi / 12 * (2 * i + 1)  # Angle
 
         # Create emotional labels
         window["-CANVAS-"].TKCanvas.create_text(
-            c[0] + r * math.sin(a),
-            c[1] - r * math.cos(a),
+            c[0] + r * np.sin(a),
+            c[1] - r * np.cos(a),
             justify=sg.tk.CENTER,
             text=lab
         )
 
     # Create emotional indicator
     emotion_indicator = window["-CANVAS-"].TKCanvas.create_oval(
-        CANVAS_WIDTH / 2 - INDICATOR_SIZE / 2,
-        CANVAS_HEIGHT / 2 - INDICATOR_SIZE / 2,
-        CANVAS_WIDTH / 2 + INDICATOR_SIZE / 2,
-        CANVAS_HEIGHT / 2 + INDICATOR_SIZE / 2
+        PRIOR_CANVAS_WIDTH / 2 - INDICATOR_SIZE / 2,
+        PRIOR_CANVAS_HEIGHT / 2 - INDICATOR_SIZE / 2,
+        PRIOR_CANVAS_WIDTH / 2 + INDICATOR_SIZE / 2,
+        PRIOR_CANVAS_HEIGHT / 2 + INDICATOR_SIZE / 2
     )
 
     return window, emotion_indicator
