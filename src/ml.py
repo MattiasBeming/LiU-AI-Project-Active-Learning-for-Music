b59df61a14e643e62641161cae2d24f85925e7f6
Mattias Beming <mattias.beming@outlook.com>
2021-11-25 11:54:26 +0100

    feat: decision tree regressor
    also:
    * fix 'KNeighborsRegressor' for  multi-output
    * fix 'LinearRegression' for  multi-output
    * refactor 'correlation' from R code to python :)

--- src/ml.py
+++ src/ml.py
@@ -3,40 +3,42 @@ import pandas as pd
 import warnings
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from transformer import *
 
 
-def k_neighbors(X, y_arousal, y_valence, k):
+def k_neighbors(X, y, k):
     """
     :param X: Feature data in numpy array with
               shape (n_timestamps*n_songs, n_features)
-    :param y_arousal: Arousal annotations belonging to the songs and timestamps
-                      in X. 1D numpy array with num_songs*time_samples entries.
-    :param y_valence: Valence annotations belonging to the songs and timestamps
-                      in X. 1D numpy array with num_songs*time_samples entries.
+    :param y: Arousal/valence annotations belonging to the songs and timestamps
+                      in X. 2D numpy array with num_songs*time_samples rows.
     :param k: Number of neighbors to look at
     :return: KNeighborsRegressor fit using X, y and k
     """
-    y = np.column_stack((y_arousal, y_valence))
-    neigh = KNeighborsRegressor(n_neighbors=k)
-    neigh = neigh.fit(X, y)
-    return neigh
+    return KNeighborsRegressor(n_neighbors=k).fit(X, y)
 
 
-def linear_regression(X, y_arousal, y_valence):
+def linear_regression(X, y):
     """
     :param X: Feature data from labeled data set, as a numpy array with
               format (num_songs*time_samples, num_features)
-    :param y_arousal: Arousal annotations belonging to the songs and timestamps
-                      in X. 1D numpy array with num_songs*time_samples entries.
-    :param y_valence: Valence annotations belonging to the songs and timestamps
-                      in X. 1D numpy array with num_songs*time_samples entries.
+    :param y: Arousal/valence annotations belonging to the songs and timestamps
+                      in X. 2D numpy array with num_songs*time_samples rows.
     :return: Linear regressors for arousal and valence
     """
-    reg_arousal = LinearRegression().fit(X, y_arousal)
-    reg_valence = LinearRegression().fit(X, y_valence)
+    return LinearRegression().fit(X, y)
 
-    return(reg_arousal, reg_valence)
+
+def decision_tree(X, y):
+    """
+    :param X: Feature data from labeled data set, as a numpy array with
+              format (num_songs*time_samples, num_features)
+    :param y: Arousal/valence annotations belonging to the songs and timestamps
+                      in X. 2D numpy array with num_songs*time_samples rows.
+    :return: Linear regressors for arousal and valence
+    """
+    return DecisionTreeRegressor().fit(X, y)
 
 
 def correlation(path_arousal, path_valence):
@@ -53,9 +55,9 @@ def correlation(path_arousal, path_valence):
             for each song. If the file paths were incorrect, this
             will be set to -1.
     """
-    if (path_arousal.is_file() and path_valence.is_file()):
+    if path_arousal.is_file() and path_valence.is_file():
         df1 = pd.read_csv(path_arousal, delimiter=",")
-        df2 = pd.read_csv(path_valence,  delimiter=",")
+        df2 = pd.read_csv(path_valence, delimiter=",")
         covs = np.zeros(df1.shape[0]-1)
         max = df1.shape[0]
         for i in range(1, max):
@@ -67,8 +69,8 @@ def correlation(path_arousal, path_valence):
         # 0.5212059045950358
         print(np.mean(covs[0:max-1]))
         # 0.2215276019990472
-        return(covs)
+        return covs
     else:
         warnings.warn(
             "Warning: One of the file paths is incorrect.")
-    return(-1)
+    return -1
