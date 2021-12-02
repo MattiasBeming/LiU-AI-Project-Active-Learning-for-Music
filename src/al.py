6684af9eae4d8dce7d60c3195e657746ec0666d7
Ted Gustafsson <ted-gustafsson@hotmail.com>
2021-12-02 11:29:38 +0100

    fix: al methods now return song_ids instead of index to samples

--- src/al.py
+++ src/al.py
@@ -3,65 +3,100 @@ import pandas as pd
 from scipy import spatial
 
 
-def max_std_sampling(pred, batchsize):
+def max_sampling_helper_(values, song_ids, batchsize, n_samples):
+    """
+    Compare sum of values for each song, finds the batchsize-max and return
+    song_ids related to those set of values.
+
+    Args:
+        values (np.array): values that are to be compared
+        song_ids (list/np.array): song_ids, must be ordered correctly to pred
+        batchsize (int): Amount of samples to be indexed for labelling
+            each epoch.
+            Note: batchsize =< unique song ids !
+        n_samples (int): number of samples in each song
+    Returns:
+        (list): song_ids which needs to be labelled
+    """
+    indexes = np.unique(song_ids, return_index=True)[1]
+    song_ids_unique = [int(song_ids[index]) for index in sorted(indexes)]
+    error_msg = f'Batchsize({batchsize} > nr songs({len(song_ids_unique)})'
+    assert batchsize <= len(song_ids_unique), error_msg
+    values_song = [np.sum(values[start:start+n_samples])
+                   for start in range(0, len(values), n_samples)]
+    print(values_song)
+    print(song_ids_unique)
+    idx = np.argpartition(values_song, -batchsize)[-batchsize:]
+    return [song_ids_unique[i] for i in idx]
+
+
+def max_std_sampling(pred, song_ids, batchsize, n_samples):
     """
     max_std_sampling implements the query by committee informativeness
     measurement.
-    Indexes batchsize samples with the predictions that have the
-    highest standard deviation(sd)
+    Indexes batchsize song_ids with the predictions that have the
+    highest standard deviation
 
-    Parameters:
-    pred - NxKxT 3d-Matrix containing predictions from every regressor for
-        every sample,
-        where N is number of samples in total.
-        k is the amount of regressors, T is targets
-    batchsize - Amount of samples to be indexed for labelling each epoch,
-        batchsize =< N
+    Args:
+        pred (np.ndarray): NxKxT 3d-Matrix containing predictions from every
+            regressor for every sample, where N is number of samples in
+            total. k is the amount of regressors, T is targets
+        song_ids (list/np.array): song_ids, must be ordered correctly to pred
+        batchsize (int): Amount of samples to be indexed for labelling
+            each epoch.
+            Note: batchsize =< unique song ids !
+        n_samples (int): number of samples in each song
 
-    Output:
-    indexes - Vector of Indexes for samples in the data which are to be
-    labelled (OBS: Not sorted)
+    Returns:
+        (list): song_ids which needs to be labelled
     """
-    error_msg = 'Batchsize must be smaller or equal to number of samples N'
-    assert batchsize <= pred.shape[0], error_msg
-    return np.argpartition(np.std(pred, axis=(1, 2)), -batchsize)[-batchsize:]
+    stds = np.std(pred, axis=(1, 2))
+    return max_sampling_helper_(stds, song_ids, batchsize, n_samples)
 
 
-###########################################################################
-# ----------------------- Output Greedy Sampling ------------------------ #
-###########################################################################
-
-
-def output_greedy_sampling(labeled_data, unlabeled_output, batch_size):
+def output_greedy_sampling(labeled_data, unlabeled_output,
+                           song_ids, batchsize, n_samples):
     """
-    :param labeled_data: Labeled data points from storage
-    :param unlabeled_output: Unlabeled data points with predictions from ML
-     model
-    :param batch_size: Number of points to query label for
-    :return: Indexes for the points to query on
-    """
+    Performs greedy sampling on outputs
 
+    Args:
+        labeled_data (np.ndarray): targets for arousal and valence
+        unlabeled_output (np.ndarray): predictions for arousal and valence
+            from ML model
+        song_ids (list/np.array): song_ids, must be ordered correctly to pred
+        batchsize (int): Amount of samples to be indexed for labelling
+            each epoch.
+            Note: batchsize =< unique song ids !
+        n_samples (int): number of samples in each song
+
+    Returns:
+        (list): song_ids which needs to be labelled
+    """
     # Compute the distance from all unlabeled output data to the closest
     # labeled data point and assign minimum one to each point
     min_distances = np.min(spatial.distance.cdist(
         unlabeled_output, labeled_data), axis=1)
-
-    # Then select the batch_size number of samples that have the largest
-    # distance to label and return indices
-    return np.argpartition(min_distances, -batch_size)[-batch_size:]
-
-
-###########################################################################
-# ------------------------ Input Greedy Sampling ------------------------ #
-###########################################################################
+    # Then select the batch_size number of songs that have the largest
+    # distance to label and return song_ids
+    return max_sampling_helper_(min_distances, song_ids, batchsize, n_samples)
 
 
-def input_greedy_sampling(labeled_feat, unlabeled_feat, batch_size):
+def input_greedy_sampling(labeled_feat, unlabeled_feat,
+                          song_ids, batchsize, n_samples):
     """
-    :param labeled_feat: Feature data from labeled set in ML
-    :param unlabeled_feat: Feature data from unlabeled set in ML
-    :param batch_size: Number of points to query label for
-    :return: Indexes for the points to query on
+    Performs greedy sampling on features(inputs)
+
+    Args:
+        labeled_feat (panda.Dataframe): features used for training
+        unlabeled_feat (panda.Dataframe): candidate features to be labelled
+        song_ids (list/np.array): list of song ids
+            ordered correctly to unlabeled_feat
+        batchsize (int): Amount of samples to be indexed for labelling
+            each epoch. Note: batchsize =< unique song ids !
+        n_samples (int): number of samples in each song
+
+    Returns:
+        (list): song_ids which needs to be labelled
     """
     # Compute the distance from all unlabeled feature data to
     # closest labeled feature data point and assign minimum one to each point
@@ -70,26 +105,31 @@ def input_greedy_sampling(labeled_feat, unlabeled_feat, batch_size):
 
     # Then select the batch_size number of samples that have the largest
     # distance to label features and return indices
-    return np.argpartition(min_distances, -batch_size)[-batch_size:]
+    return max_sampling_helper_(min_distances, song_ids, batchsize, n_samples)
 
 
-###########################################################################
-# ----------------- Input And Output Greedy Sampling -------------------- #
-###########################################################################
-
-
-def input_output_greedy_sampling(
-    labeled_feat, unlabeled_feat, labeled_data, unlabeled_output, batch_size
-):
+def input_output_greedy_sampling(labeled_feat, unlabeled_feat,
+                                 labeled_data, unlabeled_output,
+                                 song_ids, batchsize, n_samples):
     """
-    :param labeled_feat: Feature data from labeled set in ML
-    :param unlabeled_feat: Feature data from unlabeled set in ML
-    :param labeled_data: Labeled data points from storage
-    :param unlabeled_output: Unlabeled data points with predictions from ML
-    model
-    :param batch_size: Number of points to query label for
-    :return: Indexes for the points to query on
+    Performs greedy sampling on features(inputs) and outputs
+
+    Args:
+        labeled_feat (panda.Dataframe): features used for training
+        unlabeled_feat (panda.Dataframe): candidate features to be labelled
+        labeled_data (np.ndarray): targets for arousal and valence
+        unlabeled_output (np.ndarray): predictions for arousal and valence
+            from ML model
+        song_ids (list/np.array): list of song ids
+            ordered correctly to unlabeled_feat
+        batchsize (int): Amount of samples to be indexed for labelling
+            each epoch. Note: batchsize =< unique song ids !
+        n_samples (int): number of samples in each song
+
+    Returns:
+        (list): song_ids which needs to be labelled
     """
+
     # Compute the distance from all unlabeled feature data to
     # closest labeled feature data point and assign minimum one to each point
     min_distances_in = np.min(spatial.distance.cdist(
@@ -99,10 +139,9 @@ def input_output_greedy_sampling(
     # labeled data point and assign minimum one to each point
     min_distances_out = np.min(spatial.distance.cdist(
         unlabeled_output, labeled_data), axis=1)
-
     # Choose min at each index comparing both arrays
     min_distances = np.minimum(min_distances_in, min_distances_out)
 
     # Then select the batch_size number of samples that have the largest
     # distance and return indices
-    return np.argpartition(min_distances, -batch_size)[-batch_size:]
+    return max_sampling_helper_(min_distances, song_ids, batchsize, n_samples)
