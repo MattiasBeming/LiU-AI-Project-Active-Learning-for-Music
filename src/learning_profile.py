1eee5e20345ea4b6a66e0c1977b1a8246475f62e
Ludvig Joborn <ludvig.joborn@gmail.com>
2021-12-06 17:57:20 +0100

    feat: extend learning profile
    note:
    * batch_size support
    * hyper parameter support
    * MSE support for arousal and valence
    * unique id hashsum

--- src/learning_profile.py
+++ src/learning_profile.py
@@ -1,10 +1,12 @@
 from api import storage
+import numpy as np
+import hashlib
 
 
 class LearningProfile:
 
     def __init__(self, train_dataset_name: str, test_dataset_name: str,
-                 al_func, ml_func):
+                 al_func, ml_func, batch_size: int = 5):
         """
         Creates a Learning Profile. A Learning Profile is used for bundling
         train/test datasets with an Active Learning function and a Machine
@@ -21,11 +23,27 @@ class LearningProfile:
                 those found in `api/al.py`.
             ml_func (function): A Machine Learning function to use, such as
                 those found in `api/ml.py`.
+            batch_size (int): The batch size to use when performing active
+                learning. Default is 5.
         """
         self._train_dataset_name = train_dataset_name
         self._test_dataset_name = test_dataset_name
         self._al_func = al_func
         self._ml_func = ml_func
+        self._batch_size = batch_size
+        self._hyper_parameters = {}
+        self._MSE_arousal = None
+        self._MSE_valence = None
+
+    def get_id(self):
+        """
+        Returns a unique ID based on this learning profile's
+        dataset names and its al/ml functions.
+
+        Returns:
+            str: The unique ID.
+        """
+        return hashlib.md5(str(self).encode("utf-8")).hexdigest()
 
     def get_train_dataset(self):
         """
@@ -70,10 +88,81 @@ class LearningProfile:
         return self._test_dataset_name
 
     def get_al_function(self):
+        """
+        Gets the Active Learning function of this learning profile.
+
+        Returns:
+            function: The Active Learning function.
+        """
         return self._al_func
 
     def get_ml_function(self):
+        """
+        Gets the Machine Learning function of this learning profile.
+
+        Returns:
+            function: The Machine Learning function.
+        """
         return self._ml_func
+
+    def get_batch_size(self):
+        """
+        Gets the batch size of this learning profile.
+
+        Returns:
+            int: The batch size.
+        """
+        return self._batch_size
+
+    def set_hyper_parameters(self, **hyper_parameters):
+        """
+        All keyword arguments passed to this method are saved as hyper
+        parameters. These values should be used in the Machine Learning
+        function of this learning profile. See `get_ml_function()`.
+        """
+        self._hyper_parameters = hyper_parameters
+
+    def get_hyper_parameters(self):
+        """
+        Returns the hyper parameters of this learning profile, that are to be
+        used for its Machine Learning function. See `get_ml_function()`.
+
+        Returns:
+            dict: The hyper parameters on the form of a dictionary, with keys
+                as parameter names mapped to their values.
+        """
+        return self._hyper_parameters
+
+    def set_MSE(self, MSE_arousal: np.ndarray, MSE_valence: np.ndarray):
+        """
+        Sets the MSE values for this Learning Profile.
+
+        Args:
+            MSE_arousal (np.ndarray): MSE values for arousal.
+            MSE_valence (np.ndarray): MSE values for valence.
+        """
+        self._MSE_arousal = MSE_arousal
+        self._MSE_valence = MSE_valence
+
+    def get_MSE_arousal(self):
+        """
+        Gets the MSE values for arousal.
+
+        Returns:
+            np.ndarray: MSE values for arousal, or `None` if the values
+                have not yet been set through `set_MSE()`.
+        """
+        return self._MSE_arousal
+
+    def get_MSE_valence(self):
+        """
+        Gets the MSE values for valence.
+
+        Returns:
+            np.ndarray: MSE values for valence, or `None` if the values
+                have not yet been set through `set_MSE()`.
+        """
+        return self._MSE_arousal
 
     def __str__(self):
         return (
